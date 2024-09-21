7999c79f502372c9568c7f7949c9587d0bad7985
Mqzn <mezoahmed2507@gmail.com>
2024-09-22 02:33:51 +0300

    Quick fixes

--- core/src/main/java/dev/velix/imperat/help/HyphenContent.java
+++ core/src/main/java/dev/velix/imperat/help/HyphenContent.java
@@ -2,11 +2,7 @@ package dev.velix.imperat.help;
 
 import dev.velix.imperat.command.Command;
 import dev.velix.imperat.context.Source;
-import lombok.Getter;
-import lombok.experimental.Accessors;
 
-@Getter
-@Accessors(fluent = true)
 public record HyphenContent<S extends Source>(Command<S> command, int currentPage, int maxPages) {
     public static <S extends Source> HyphenContent<S> of(Command<S> cmd, int currentPage, int maxPages) {
         return new HyphenContent<>(cmd, currentPage, maxPages);
