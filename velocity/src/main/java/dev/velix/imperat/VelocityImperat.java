d36e73cfe4ec20a27df2ed57efcc7dee8bea47ef
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2024-09-24 14:50:41 +0300

    Add a Player suggestion for velocity

--- velocity/src/main/java/dev/velix/imperat/VelocityImperat.java
+++ velocity/src/main/java/dev/velix/imperat/VelocityImperat.java
@@ -5,10 +5,13 @@ import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.plugin.PluginContainer;
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import dev.velix.imperat.command.BaseImperat;
 import dev.velix.imperat.command.Command;
+import dev.velix.imperat.exception.UnknownPlayerException;
 import dev.velix.imperat.resolvers.PermissionResolver;
+import dev.velix.imperat.resolvers.SuggestionResolver;
 import dev.velix.imperat.util.Preconditions;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,6 +32,8 @@ public final class VelocityImperat extends BaseImperat<VelocitySource> {
         this.plugin = proxyServer.getPluginManager().getPlugin(pluginName).orElseThrow(() -> new IllegalStateException("Unknown plugin with name '" + pluginName + "'"));
         this.proxyServer = proxyServer;
         this.velocityBrigadier = VelocityBrigadier.create(this);
+
+        this.registerDefaultResolvers();
     }
     
     public static VelocityImperat create(
@@ -45,8 +50,24 @@ public final class VelocityImperat extends BaseImperat<VelocitySource> {
     ) {
         return create(pluginName, proxyServer, (source, permission) -> source.origin().hasPermission(permission));
     }
-    
-    
+
+    private void registerDefaultResolvers() {
+        // Player
+        this.registerValueResolver(Player.class, (context, parameter, cursor, raw) -> {
+            if (raw.equalsIgnoreCase("me")) {
+                if (context.source().isConsole()) {
+                    throw new UnknownPlayerException(raw);
+                }
+                return context.source().asPlayer();
+            }
+            return proxyServer.getPlayer(raw.toLowerCase()).orElseThrow(() -> new UnknownPlayerException(raw));
+        });
+
+        this.registerSuggestionResolver(
+                SuggestionResolver.plain(Player.class, proxyServer.getAllPlayers().stream().map(Player::getUsername).toList())
+        );
+    }
+
     @Override
     public void registerCommand(Command<VelocitySource> command) {
         super.registerCommand(command);
