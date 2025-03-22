9a72b98e6dae2157f149f39c101e5979b1c6ee5b
Mqzn <mezoahmed2507@gmail.com>
2025-03-23 00:54:23 +0200

    Improved old testing code

--- bukkit/src/main/java/dev/velix/imperat/InternalBukkitCommand.java
+++ bukkit/src/main/java/dev/velix/imperat/InternalBukkitCommand.java
@@ -3,7 +3,6 @@ package dev.velix.imperat;
 import dev.velix.imperat.command.Command;
 import dev.velix.imperat.command.CommandUsage;
 import dev.velix.imperat.util.ImperatDebugger;
-import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginIdentifiableCommand;
 import org.bukkit.plugin.Plugin;
@@ -74,45 +73,23 @@ final class InternalBukkitCommand extends org.bukkit.command.Command implements 
 
     }
 
-    public @NotNull List<String> tabComplete0(
+    @Override
+    public @NotNull List<String> tabComplete(
         final @NotNull CommandSender sender,
+        final @NotNull String alias,
         final String[] args
     ) throws IllegalArgumentException {
         //ImperatDebugger.debug("Starting bukkit tab-completion");
         BukkitSource source = dispatcher.wrapSender(sender);
         try {
             //ImperatDebugger.debug("Size of completions= %s", completions.size());
-            List<String> completions = dispatcher.autoComplete(command, source, args).join();
-            ImperatDebugger.debug("Completions= " + String.join(",", completions));
-            return completions;
+            return dispatcher.autoComplete(command, source, args).join();
         } catch (Exception ex) {
             ImperatDebugger.error(InternalBukkitCommand.class, "tabComplete", ex);
             return Collections.emptyList();
         }
     }
 
-
-    @Override
-    public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws CommandException, IllegalArgumentException {
-        List<String> completions;
-
-        try {
-            completions = this.tabComplete0(sender, args);
-
-        } catch (Throwable ex) {
-            StringBuilder message = new StringBuilder();
-            message.append("Unhandled exception during tab completion for command '/").append(alias).append(' ');
-
-            for(String arg : args) {
-                message.append(arg).append(' ');
-            }
-
-            message.deleteCharAt(message.length() - 1).append("' in plugin ").append(this.getPlugin().getDescription().getFullName());
-            throw new CommandException(message.toString(), ex);
-        }
-
-        return completions.isEmpty() ? super.tabComplete(sender, alias, args) : completions;
-    }
 
 
 }
