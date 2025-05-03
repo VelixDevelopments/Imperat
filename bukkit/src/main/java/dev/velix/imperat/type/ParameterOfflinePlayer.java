07b5bcb7b9744f3fac902be3ecb75f5a893fc96e
Mqzn <mezoahmed2507@gmail.com>
2025-05-03 18:55:35 +0300

    Refactor ParameterOfflinePlayer to simplify player resolution logic

--- bukkit/src/main/java/dev/velix/imperat/type/ParameterOfflinePlayer.java
+++ bukkit/src/main/java/dev/velix/imperat/type/ParameterOfflinePlayer.java
@@ -1,7 +1,5 @@
 package dev.velix.imperat.type;
 
-import com.google.common.base.Charsets;
-import com.mojang.authlib.GameProfile;
 import dev.velix.imperat.BukkitSource;
 import dev.velix.imperat.command.parameters.CommandParameter;
 import dev.velix.imperat.command.parameters.type.BaseParameterType;
@@ -9,21 +7,16 @@ import dev.velix.imperat.context.ExecutionContext;
 import dev.velix.imperat.context.SuggestionContext;
 import dev.velix.imperat.context.internal.CommandInputStream;
 import dev.velix.imperat.exception.ImperatException;
-import dev.velix.imperat.exception.UnknownOfflinePlayerException;
 import dev.velix.imperat.exception.UnknownPlayerException;
 import dev.velix.imperat.resolvers.SuggestionResolver;
 import dev.velix.imperat.util.TypeWrap;
-import net.minecraft.server.v1_13_R2.MinecraftServer;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.craftbukkit.v1_13_R2.CraftServer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.spigotmc.SpigotConfig;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 public class ParameterOfflinePlayer extends BaseParameterType<BukkitSource, OfflinePlayer> {
 
@@ -40,33 +33,16 @@ public class ParameterOfflinePlayer extends BaseParameterType<BukkitSource, Offl
     }
 
     @Override
-    @SuppressWarnings("deprecation")
     public @Nullable OfflinePlayer resolve(
             @NotNull ExecutionContext<BukkitSource> context,
             @NotNull CommandInputStream<BukkitSource> commandInputStream,
             String input) throws ImperatException {
-        String name = commandInputStream.currentRaw().orElse(null);
-        if (name == null) return null;
 
-        if (name.length() > 16) {
-            throw new UnknownPlayerException(name);
+        if (input.length() > 16) {
+            throw new UnknownPlayerException(input);
         }
 
-        GameProfile profile = null;
-        if (MinecraftServer.getServer().getOnlineMode() || SpigotConfig.bungee) {
-            profile = MinecraftServer.getServer().getUserCache().getProfile(name);
-        }
-
-        if (profile == null) {
-
-            if (!ifCachedOnly) {
-                profile = new GameProfile(UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(Charsets.UTF_8)), name);
-            } else {
-                throw new UnknownOfflinePlayerException("Unknown offline player");
-            }
-        }
-
-        return ((CraftServer) Bukkit.getServer()).getOfflinePlayer(profile);
+        return Bukkit.getOfflinePlayer(input);
     }
 
     @Override
