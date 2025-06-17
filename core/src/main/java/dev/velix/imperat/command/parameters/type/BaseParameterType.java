4298d0f8380e84c7e1aa77a50848cdcb7f1f0453
Mqzn <mezoahmed2507@gmail.com>
2025-06-17 22:02:25 +0300

    Add TypeWrap import to improve docs

--- core/src/main/java/dev/velix/imperat/command/parameters/type/BaseParameterType.java
+++ core/src/main/java/dev/velix/imperat/command/parameters/type/BaseParameterType.java
@@ -7,6 +7,7 @@ import dev.velix.imperat.context.ArgumentQueue;
 import dev.velix.imperat.context.Source;
 import dev.velix.imperat.resolvers.SuggestionResolver;
 import dev.velix.imperat.util.TypeCapturer;
+import dev.velix.imperat.util.TypeWrap;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
