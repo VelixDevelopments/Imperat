ee8449f76e52e95b7c4af91f9e86fc0dbd027e2f
Ahmed Waleed <61851106+iiAhmedYT@users.noreply.github.com>
2025-06-17 19:44:53 +0300

    Update MapParameterType.java

--- core/src/main/java/dev/velix/imperat/command/parameters/type/MapParameterType.java
+++ core/src/main/java/dev/velix/imperat/command/parameters/type/MapParameterType.java
@@ -24,7 +24,7 @@ public class MapParameterType<S extends Source, K, V, M extends Map<K, V>> exten
             ParameterType<S, K> keyResolver,
             ParameterType<S, V> valueResolver
     ) {
-        super();
+        super(this.extractType(4));
         this.mapInitializer = mapInitializer;
         this.keyResolver = keyResolver;
         this.valueResolver = valueResolver;
